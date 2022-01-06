a2d8b0ad4d69d698f7dcebc4aec7fcaf50fd3884
Tehila <Tehila>
2022-01-06 19:27:05 +0200

    yes

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,53 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Shop shop = new Shop();
         String userNameToCheck;
         String passwordToCheck;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your username: ");
-        userNameToCheck = scanner.nextLine();
-        System.out.println("Enter your password: ");
-        passwordToCheck = scanner.nextLine();
+        int employeeOrCustomer;
+        int userChoice;
+
+        do {
+            System.out.println("1 - Create a new account\n" +
+                    "2 - Login to an existing account\n" +
+                    "3 - Exit");
+            userChoice = scanner.nextInt();
+        }while (userChoice != 1 && userChoice != 2 && userChoice != 3);
+
+        if (userChoice == 1){
+            shop.crateAccount();
+        }
+
+        if (userChoice == 2){
+            System.out.println("--Logging in--");
+            do {
+                System.out.println("Are you interested in logging in to an employee's or customer's account" +
+                        "\n" + "for employee enter 1, for customer enter 2");
+                employeeOrCustomer = scanner.nextInt();
+            }while (employeeOrCustomer != 1 && employeeOrCustomer != 2);
+
+            System.out.println("Enter your username: ");
+            userNameToCheck = scanner.nextLine();
+            System.out.println("Enter your password: ");
+            passwordToCheck = scanner.nextLine();
+            if (employeeOrCustomer == 1) {
+
+            }else {
+                shop.signInCustomer(userNameToCheck,passwordToCheck);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
--- src/Shop.java
+++ src/Shop.java
@@ -6,11 +6,7 @@ public class Shop {
     private Product[] products;
 
 
-
-
-
-
-    public void crateAccount (){
+    public void crateAccount() {
         Scanner scanner = new Scanner(System.in);
 
         String firstName;
@@ -18,51 +14,82 @@ public class Shop {
         String userName;
         String password;
         int userType;
-
-        boolean isStrongPassword;
         boolean userNameIsExist;
-        boolean isEstaCostumeralse;
+        boolean isCostumer;
 
-        System.out.println("-press 1 for costumer"+"\n" +"-press 2 for Employee" );
-        userType = scanner.nextInt();
+        do {
+            System.out.println("-press 1 for costumer" + "\n" + "-press 2 for Employee");
+            userType = scanner.nextInt();
+        } while (userType != 1 && userType != 2);
+
+        if (userType == 1) {
+            isCostumer = true;
+        } else {
+            isCostumer = false;
+        }
+
+        //הסקנר הבא לא עובד בלי זה משום מה, הוא מדלג על הראשון
+        userName = scanner.nextLine();
+
+
         do {
             System.out.println("Enter your first name: ");
             firstName = scanner.nextLine();
-        }while (firstName.equals("uyuyu"));
+        } while (hasDigit(firstName));
 
 
-        System.out.println("Enter your last Name : ");
-        lastName = scanner.nextLine();
+        do {
+            System.out.println("Enter your last Name : ");
+            lastName = scanner.nextLine();
+        } while (hasDigit(lastName));
 
+        System.out.println("Enter username: ");
+        userName = scanner.nextLine();
 
-
-
+        do {
+            System.out.println("Enter password with 6 chars: ");
+            password = scanner.nextLine();
+        }while (password.length() < 6);
     }
 
 
+//
+//    public Employee signInEmployee (String usernameToCheck,String passwordToCheck) {
+//        for (int i = 0; i < this.employees.length; i++) {
+//            Customer currentEmployee = this.employees[i];
+//            if (currentEmployee.getUsername().equals(usernameToCheck) &&
+//                    currentEmployee.getPassword().equals(passwordToCheck)) {
+//                return currentEmployee;
+//            }
+//        }
+//        return null;
+//    }
 
-    public Customer signUp (){
-        boolean isCustomer = false;
-        System.out.println("--Logging in--");
-        Scanner scanner = new Scanner(System.in);
-        String userNameToCheck;
-        String passwordToCheck;
 
-
-        System.out.println("Enter your username: ");
-        userNameToCheck = scanner.nextLine();
-        System.out.println("Enter your password: ");
-        passwordToCheck = scanner.nextLine();
-
+    public Customer signInCustomer(String usernameToCheck, String passwordToCheck) {
 
         for (int i = 0; i < this.customers.length; i++) {
             Customer currentCustomer = this.customers[i];
-            if (currentCustomer.getUsername().equals(userNameToCheck) &&
+            if (currentCustomer.getUsername().equals(usernameToCheck) &&
                     currentCustomer.getPassword().equals(passwordToCheck)) {
                 return currentCustomer;
             }
         }
-
         return null;
     }
+
+    private boolean hasDigit(String stringToCheck) {
+        boolean hasDigit = false;
+        for (int i = 0; i < stringToCheck.length(); i++) {
+            if (Character.isDigit(stringToCheck.charAt(i))) {
+                hasDigit = true;
+                break;
+            }
+        }
+        return hasDigit;
+    }
+
+
+
+
 }
